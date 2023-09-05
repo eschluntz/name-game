8500d31aed3ad9db5be271956ff64a5faa4f626f
Erik Schluntz <eschluntz@gmail.com>
2023-09-05 14:39:16 -0700

    wip

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <top-bar></top-bar>
+  <face-card></face-card>
+  <user-experiences></user-experiences>
+</template>
+
+<script>
+import FaceCard from './components/FaceCard.vue';
+import UserExperiences from './components/UserExperiences.vue';
+import TopBar from './components/TopBar.vue';
+
+export default {
+  components: {
+    FaceCard,
+    UserExperiences,
+    TopBar,
+  },
+  // data() {
+  //   return {
+  //     savedSurveyResults: [],
+  //   };
+  // },
+  // methods: {
+  //   storeSurvey(surveyData) {
+  //     const surveyResult = {
+  //       name: surveyData.userName,
+  //       rating: surveyData.rating,
+  //       id: new Date().toISOString(),
+  //     };
+  //     this.savedSurveyResults.push(surveyResult);
+  //     console.log(surveyResult);
+  //   },
+  // },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #360032;
+  background-color: #360032;
+  color: white;
+  padding: 0.5rem 2rem;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #5c0556;
+  border-color: #5c0556;
+}
+
+div .card {
+  margin: 2rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+
+</style>
---
+++ src/components/FaceCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+      <div class="card">
+        <img src="https://www.ycombinator.com/assets/ycdc/people/paulg-8ca9fa356bb6e7e3e21078a18d8823a5ea393808ef56be2d7d5e60b83be790af.jpg"/>
+        <input type="text" id="name" name="name" v-model.trim="enteredName" />
+        
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        enteredName: '',
+        faceUrl: '',
+      };
+    },
+    methods: {
+      submitSurvey() {}
+    }
+  };
+  </script>
+  
+  <style scoped>
+  img {
+    width: 20rem;
+  }
+  
+  input[type='text'] {
+    display: block;
+    width: 20rem;
+    margin-top: 0.5rem;
+  }
+  </style>
