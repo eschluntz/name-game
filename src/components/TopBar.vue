655d27afd1ae22b34c417d93e290cf3c45168423
Erik Schluntz <eschluntz@gmail.com>
2023-09-07 12:53:11 -0700

    formatting

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,47 +1,42 @@
 <template>
-    <section>
-        <div class="title">Silicon Valley Name Game</div>
-            <table><tr>
-                <td>
-                    <button>Top VCs</button>
-                </td>
-                <td>
-                    <button>Top Tech CEOs</button>
-                </td>
-                <td>
-                    <button>YC Partners</button>
-                </td>
-                <td>
-                    <button>Create your own List</button>
-                </td>
-            </tr></table>
-    </section>
+  <section>
+    <div class="title">Silicon Valley Name Game</div>
+    <table>
+      <tr>
+        <td>
+          <button>Top VCs</button>
+        </td>
+        <td>
+          <button>Top Tech CEOs</button>
+        </td>
+        <td>
+          <button>YC Partners</button>
+        </td>
+        <td>
+          <button>Create your own List</button>
+        </td>
+      </tr>
+    </table>
+  </section>
 </template>
   
 <script>
-    // TODO: highlight which one is selected via a prop
-    export default {
-    data() {
-        return {};
-    },
-    // emits: ['survey-submit'],
-    methods: {
-        submitSurvey() {}
-        }
-    }
+export default {}
 </script>
 
 <style scoped>
-    section {
-        background-color: bisque;
-        text-align: center;
-        padding-bottom: 1rem;
-    }
-    table {
-        margin-left: auto;
-        margin-right: auto;
-    }
-    .title {
-        font-size:xx-large;
-    }
+section {
+  background-color: bisque;
+  text-align: center;
+  padding-bottom: 1rem;
+}
+
+table {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.title {
+  font-size: xx-large;
+}
 </style>
